4ea628f5cdc12641b9626d32b58e4a2c62c9a812
Amir Tuval <amirtu@gmail.com>
2014-05-28 21:48:33 +0300

    Basic project structure

---
+++ libmysqlhll-test/test.cxx
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "mysqlhll.h"
+
+int main(int argc, char** argv) {
+  printf("%s\n", getMessage());
+  return 0;
+}
---
+++ libmysqlhll/mysqlhll.cxx
@@ -0,0 +1,3 @@
+const char* getMessage() {
+  return "Hello World!";
+}
