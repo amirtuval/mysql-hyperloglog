fca1b1a777fba87402a77afea5210a55e5e8401d
Amir Tuval <amirtu@gmail.com>
2014-06-14 22:16:34 +0300

    Add SerializedHyperLogLog class, serializing to/from base64

---
+++ libmysqlhll/SerializedHyperLogLog.hpp
@@ -0,0 +1,43 @@
+#if !defined(SERIALIZED_HYPERLOGLOG_HPP)
+#define SERIALIZED_HYPERLOGLOG_HPP
+
+#include <stdio.h>
+#include <string.h>
+
+#include "hyperloglog.hpp"
+#include "base64.h"
+
+class SerializedHyperLogLog : public hll::HyperLogLog {
+
+public:
+    SerializedHyperLogLog(uint8_t b) : hll::HyperLogLog::HyperLogLog(b) {
+    }
+
+    char* toString(char* result) {
+      size_t outputLength;
+      char* mEncoded = base64_encode(&M_[0], M_.size(), &outputLength);
+      sprintf(result, "%d|%s", b_, mEncoded);
+      free(mEncoded);
+      return result;
+    }
+
+    static SerializedHyperLogLog* fromString(char* encoded) {
+      int m;
+      char base64[10000];
+      sscanf(encoded, "%d|%s", &m, base64);
+      
+      SerializedHyperLogLog* result = new SerializedHyperLogLog(m);
+
+      size_t outputLength;
+      unsigned char* decoded = base64_decode(base64, strlen(base64), &outputLength);
+
+      for(int i = 0; i < outputLength; ++i) {
+         result->M_[i] = decoded[i];
+      }
+      
+      return result;
+    }
+
+};
+
+#endif
