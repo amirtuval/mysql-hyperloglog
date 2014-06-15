eac8927f9f0b5fe7230f924d733e17d29c5c1466
Amir Tuval <amirtu@gmail.com>
2014-06-15 22:08:07 +0300

    add initial aggregate functions hll_create and hll_compute

--- libmysqlhll-test/test.cxx
+++ libmysqlhll-test/test.cxx
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#include "mysqlhll.h"
+//#include "mysqlhll.h"
 
 int main(int argc, char** argv) {
-  printf("%s\n", getMessage());
+//  printf("%s\n", getMessage());
   return 0;
 }
--- libmysqlhll/mysqlhll.cxx
+++ libmysqlhll/mysqlhll.cxx
@@ -1,5 +1,115 @@
+#include <mysql.h>
+
 #include "SerializedHyperLogLog.hpp"
 
-const char* getMessage() {
-  return "Hello World!";
+extern "C" {
+
+#define LOG(...) fprintf(stderr, __VA_ARGS__);
+
+my_bool hll_create_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+void hll_create_deinit(UDF_INIT *initid);
+char *hll_create(UDF_INIT *initid, UDF_ARGS *args, char *result,
+          unsigned long *length, char *is_null, char *error);
+void hll_create_clear(UDF_INIT* initid, char* is_null, char* message);
+void hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+
+my_bool hll_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+void hll_compute_deinit(UDF_INIT *initid);
+long long hll_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
+          unsigned long *length, char *is_null, char *error);
+void hll_compute_clear(UDF_INIT* initid, char* is_null, char* message);
+void hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+
+class Data {
+  public: 
+    SerializedHyperLogLog* shll;
+    char* result;
+
+    Data() {
+      shll = new SerializedHyperLogLog(12);
+      result = (char*)malloc(10000);
+    }
+
+    ~Data() {
+      delete shll;
+      free(result);
+    }
+};
+
+my_bool hll_create_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+
+  if (args->arg_count == 0) {
+    strcpy(message,"Wrong arguments to HLL();  Must have at least 1 argument");
+    return 1;
+  }
+
+  for(int i = 0; i < args->arg_count; ++i) {
+    args->arg_type[i] = STRING_RESULT;
+  }
+
+  initid->ptr = (char*)new Data();
+  return 0;
+}
+
+Data* data(UDF_INIT *initid) {
+  return (Data*)initid->ptr;
+}
+
+SerializedHyperLogLog* shll(UDF_INIT *initid) {
+  return data(initid)->shll;
+}
+
+void hll_create_deinit(UDF_INIT *initid) {
+  delete data(initid);
+}
+
+char *hll_create(UDF_INIT *initid, UDF_ARGS *args, char *result, 
+          unsigned long *length, char *is_null, char *error) {
+
+  char* hll_result = data(initid)->result;
+  shll(initid)->toString(hll_result);
+  *length = strlen(hll_result);
+
+  LOG("hll %s\n", hll_result);
+
+  return hll_result;
+}
+
+void hll_create_clear(UDF_INIT* initid, char* is_null, char* message) {
+  LOG("hll clear\n");
+  shll(initid)->clear();
+}
+
+void hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
+  for(int i = 0; i < args->arg_count; ++i) {
+    LOG("hll_add %*s %d\n", (int)args->lengths[i], args->args[i], (int)args->lengths[i]);
+
+    shll(initid)->add(args->args[i], args->lengths[i]);
+  }
+}
+
+my_bool hll_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+  return hll_create_init(initid, args, message);
+}
+
+void hll_compute_deinit(UDF_INIT *initid) {
+  return hll_create_deinit(initid);
+}
+
+long long hll_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
+          unsigned long *length, char *is_null, char *error) {
+  LOG("hll_compute\n");
+  return shll(initid)->estimate();
+}
+
+void hll_compute_clear(UDF_INIT* initid, char* is_null, char* message) {
+  LOG("hll_compute_clear\n");
+  hll_create_clear(initid, is_null, message);
+}
+
+void hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
+  LOG("hll_compute_add\n");
+  hll_create_add(initid, args, is_null, message);
+}
+
 }
