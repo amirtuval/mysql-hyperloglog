e41c057bcf7e69d9f5f74349341335985905d711
Amir Tuval <amirtu@gmail.com>
2014-06-16 21:32:44 +0300

    add udf.sql script to create aggregate functions

---
+++ sql/udf.sql
@@ -0,0 +1,11 @@
+# rename to '.so' to '.dll' on windows
+
+drop function if exists hll_create;
+drop function if exists hll_compute;
+drop function if exists hll_merge;
+drop function if exists hll_merge_compute;
+
+create aggregate function hll_create returns string soname 'libmysqlhll.so';
+create aggregate function hll_compute returns int soname 'libmysqlhll.so';
+create aggregate function hll_merge returns string soname 'libmysqlhll.so';
+create aggregate function hll_merge_compute returns int soname 'libmysqlhll.so';
