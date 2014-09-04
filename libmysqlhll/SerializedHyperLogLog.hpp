33c102978bc60cc7db6a4163ff94bfe1e08abe0f
Vivek Hariharan <vivek.hariharan@administrators-MacBook-Pro-2.local>
2014-09-04 11:53:41 -0700

    Changed the logic to allocate the char array for the base64 string

--- libmysqlhll/SerializedHyperLogLog.hpp
+++ libmysqlhll/SerializedHyperLogLog.hpp
@@ -3,48 +3,58 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #include "hyperloglog.hpp"
 #include "base64.h"
 
-class SerializedHyperLogLog : public hll::HyperLogLog {
+class SerializedHyperLogLog: public hll::HyperLogLog {
 
 public:
-    SerializedHyperLogLog(uint8_t b) : HyperLogLog(b) {
-    }
+	SerializedHyperLogLog(uint8_t b) :
+			HyperLogLog(b) {
+	}
 
-    char* toString(char* result) {
-      size_t outputLength;
-      char* mEncoded = base64_encode(&M_[0], M_.size(), &outputLength);
-      sprintf(result, "%d|%s", b_, mEncoded);
-      free(mEncoded);
-      return result;
-    }
+	char* toString(char* result) {
+		size_t outputLength;
+		char* mEncoded = base64_encode(&M_[0], M_.size(), &outputLength);
+		sprintf(result, "%d|%s", b_, mEncoded);
+		free(mEncoded);
+		return result;
+	}
 
-    static SerializedHyperLogLog* fromString(char* encoded) {
-      if (encoded == NULL) return NULL;
-      if (strlen(encoded) < 3) return NULL;
-      if (strchr(encoded, '|') == NULL) return NULL;
+	static SerializedHyperLogLog* fromString(char* encoded) {
+		if (encoded == NULL)
+			return NULL;
+		if (strlen(encoded) < 3)
+			return NULL;
+		if (strchr(encoded, '|') == NULL)
+			return NULL;
 
-      int m;
-      char base64[10000];
-      sscanf(encoded, "%d|%s", &m, base64);
-      
-      SerializedHyperLogLog* result = new SerializedHyperLogLog(m);
+		int m;
+		int base64StringLength = ceil((1 << m) / 3) * 4;
+		//add padding
+		base64StringLength += 10;
+		char base64[base64StringLength];
+		sscanf(encoded, "%d|%s", &m, base64);
 
-      size_t outputLength;
-      unsigned char* decoded = base64_decode(base64, strlen(base64), &outputLength);
+		SerializedHyperLogLog* result = new SerializedHyperLogLog(m);
 
-      if (decoded == NULL) return NULL;
+		size_t outputLength;
+		unsigned char* decoded = base64_decode(base64, strlen(base64),
+				&outputLength);
 
-      for(int i = 0; i < outputLength; ++i) {
-         result->M_[i] = decoded[i];
-      }
+		if (decoded == NULL)
+			return NULL;
 
-      free(decoded);
-      
-      return result;
-    }
+		for (int i = 0; i < outputLength; ++i) {
+			result->M_[i] = decoded[i];
+		}
+
+		free(decoded);
+
+		return result;
+	}
 
 };
 
--- libmysqlhll/mysqlhll.cxx
+++ libmysqlhll/mysqlhll.cxx
@@ -1,12 +1,13 @@
 #ifdef WIN32
-  #include <winsock.h>
-  typedef signed char int8_t;
-  #define EXPORT __declspec(dllexport)
+#include <winsock.h>
+typedef signed char int8_t;
+#define EXPORT __declspec(dllexport)
 #else
-  #define EXPORT
+#define EXPORT
 #endif
 
 #include <mysql.h>
+#include <math.h>
 
 #include "constants.hpp"
 #include "SerializedHyperLogLog.hpp"
@@ -14,227 +15,258 @@
 extern "C" {
 
 #ifndef NDEBUG
- #define LOG(...) fprintf(stderr, __VA_ARGS__);
+#define LOG(...) fprintf(stderr, __VA_ARGS__);
 #else
- #define LOG(...)
+#define LOG(...)
 #endif
 
 my_bool EXPORT hll_create_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 void EXPORT hll_create_deinit(UDF_INIT *initid);
 char EXPORT *hll_create(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error);
+		unsigned long *length, char *is_null, char *error);
 void EXPORT hll_create_clear(UDF_INIT* initid, char* is_null, char* message);
-void EXPORT hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+void EXPORT hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message);
 
-my_bool EXPORT hll_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+my_bool EXPORT hll_compute_init(UDF_INIT *initid, UDF_ARGS *args,
+		char *message);
 void EXPORT hll_compute_deinit(UDF_INIT *initid);
 long long EXPORT hll_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error);
+		unsigned long *length, char *is_null, char *error);
 void EXPORT hll_compute_clear(UDF_INIT* initid, char* is_null, char* message);
-void EXPORT hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+void EXPORT hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message);
 
 my_bool EXPORT hll_merge_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 void EXPORT hll_merge_deinit(UDF_INIT *initid);
 char EXPORT *hll_merge(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error);
+		unsigned long *length, char *is_null, char *error);
 void EXPORT hll_merge_clear(UDF_INIT* initid, char* is_null, char* message);
-void EXPORT hll_merge_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+void EXPORT hll_merge_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message);
 
-my_bool EXPORT hll_merge_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+my_bool EXPORT hll_merge_compute_init(UDF_INIT *initid, UDF_ARGS *args,
+		char *message);
 void EXPORT hll_merge_compute_deinit(UDF_INIT *initid);
-long long EXPORT hll_merge_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error);
-void EXPORT hll_merge_compute_clear(UDF_INIT* initid, char* is_null, char* message);
-void EXPORT hll_merge_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message);
+long long EXPORT hll_merge_compute(UDF_INIT *initid, UDF_ARGS *args,
+		char *result, unsigned long *length, char *is_null, char *error);
+void EXPORT hll_merge_compute_clear(UDF_INIT* initid, char* is_null,
+		char* message);
+void EXPORT hll_merge_compute_add(UDF_INIT* initid, UDF_ARGS* args,
+		char* is_null, char* message);
 
 class Data {
-  public: 
-    SerializedHyperLogLog* shll;
-    char* result;
+public:
+	SerializedHyperLogLog* shll;
+	char* result;
 
-    Data(bool need_result) {
-      shll = new SerializedHyperLogLog(HLL_BIT_WIDTH);
+	Data(bool need_result) {
+		shll = new SerializedHyperLogLog(HLL_BIT_WIDTH);
 
-      if (need_result) {
-        result = (char*)malloc(10000);
-      } else {
-        result = NULL;
-      }
-    }
+		int twoPowerB
+		int bytesToAllocate = ceil((1 << HLL_BIT_WIDTH) / 3) * 4; //the base64 string is 4*ceil(n/3) where n is the number of bytes in the original string
+		//add padding
+		bytesToAllocate += 10;
 
-    ~Data() {
-      delete shll;
-      if (result != NULL) {
-        free(result);
-      }
-    }
+		if (need_result) {
+			result = (char*) malloc(bytesToAllocate);
+		} else {
+			result = NULL;
+		}
+	}
+
+	~Data() {
+		delete shll;
+		if (result != NULL) {
+			free(result);
+		}
+	}
 };
 
-my_bool init(UDF_INIT *initid, UDF_ARGS *args, char *message, bool need_result, const char* function_name) {
-  if (args->arg_count == 0) {
-    sprintf(message, "Wrong arguments to %s();  Must have at least 1 argument", function_name);
-    return 1;
-  }
+my_bool init(UDF_INIT *initid, UDF_ARGS *args, char *message, bool need_result,
+		const char* function_name) {
+	if (args->arg_count == 0) {
+		sprintf(message,
+				"Wrong arguments to %s();  Must have at least 1 argument",
+				function_name);
+		return 1;
+	}
 
-  for(int i = 0; i < args->arg_count; ++i) {
-    args->arg_type[i] = STRING_RESULT;
-  }
+	for (int i = 0; i < args->arg_count; ++i) {
+		args->arg_type[i] = STRING_RESULT;
+	}
 
-  initid->ptr = (char*)new Data(need_result);
-  return 0;
+	initid->ptr = (char*) new Data(need_result);
+	return 0;
 }
 
-my_bool EXPORT hll_create_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
-  return init(initid, args, message, true, "HLL_CREATE");
+my_bool EXPORT hll_create_init(UDF_INIT *initid, UDF_ARGS *args,
+		char *message) {
+	return init(initid, args, message, true, "HLL_CREATE");
 }
 
 Data* data(UDF_INIT *initid) {
-  return (Data*)initid->ptr;
+	return (Data*) initid->ptr;
 }
 
 SerializedHyperLogLog* shll(UDF_INIT *initid) {
-  return data(initid)->shll;
+	return data(initid)->shll;
 }
 
 void EXPORT hll_create_deinit(UDF_INIT *initid) {
-  delete data(initid);
+	delete data(initid);
 }
 
-char EXPORT *hll_create(UDF_INIT *initid, UDF_ARGS *args, char *result, 
-          unsigned long *length, char *is_null, char *error) {
+char EXPORT *hll_create(UDF_INIT *initid, UDF_ARGS *args, char *result,
+		unsigned long *length, char *is_null, char *error) {
 
-  char* hll_result = data(initid)->result;
-  shll(initid)->toString(hll_result);
-  *length = strlen(hll_result);
+	char* hll_result = data(initid)->result;
+	shll(initid)->toString(hll_result);
+	*length = strlen(hll_result);
 
-  LOG("hll %s\n", hll_result);
+	LOG("hll %s\n", hll_result);
 
-  return hll_result;
+	return hll_result;
 }
 
 void EXPORT hll_create_clear(UDF_INIT* initid, char* is_null, char* message) {
-  LOG("hll clear\n");
-  shll(initid)->clear();
+	LOG("hll clear\n");
+	shll(initid)->clear();
 }
 
-void get_value_and_length(UDF_ARGS* args, int i, const char** value, uint32_t* length) {
-    *value = (args->args[i] == NULL ? "" : args->args[i]);
-    *length = (args->args[i] == NULL ? 0 : args->lengths[i]);
+void get_value_and_length(UDF_ARGS* args, int i, const char** value,
+		uint32_t* length) {
+	*value = (args->args[i] == NULL ? "" : args->args[i]);
+	*length = (args->args[i] == NULL ? 0 : args->lengths[i]);
 }
 
-void EXPORT hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
-  for(int i = 0; i < args->arg_count; ++i) {
-    const char* value;
-    uint32_t length;
-    get_value_and_length(args, i, &value, &length);
-    shll(initid)->add(value, length);
-  }
+void EXPORT hll_create_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message) {
+	for (int i = 0; i < args->arg_count; ++i) {
+		const char* value;
+		uint32_t length;
+		get_value_and_length(args, i, &value, &length);
+		shll(initid)->add(value, length);
+	}
 }
 
-my_bool EXPORT hll_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
-  return init(initid, args, message, false, "HLL_COMPUTE");
+my_bool EXPORT hll_compute_init(UDF_INIT *initid, UDF_ARGS *args,
+		char *message) {
+	return init(initid, args, message, false, "HLL_COMPUTE");
 }
 
 void EXPORT hll_compute_deinit(UDF_INIT *initid) {
-  return hll_create_deinit(initid);
+	return hll_create_deinit(initid);
 }
 
 long long EXPORT hll_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error) {
-  LOG("hll_compute\n");
-  return shll(initid)->estimate();
+		unsigned long *length, char *is_null, char *error) {
+	LOG("hll_compute\n");
+	return shll(initid)->estimate();
 }
 
 void EXPORT hll_compute_clear(UDF_INIT* initid, char* is_null, char* message) {
-  LOG("hll_compute_clear\n");
-  hll_create_clear(initid, is_null, message);
+	LOG("hll_compute_clear\n");
+	hll_create_clear(initid, is_null, message);
 }
 
-void EXPORT hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
-  LOG("hll_compute_add\n");
-  hll_create_add(initid, args, is_null, message);
+void EXPORT hll_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message) {
+	LOG("hll_compute_add\n");
+	hll_create_add(initid, args, is_null, message);
 }
 
-my_bool merge_init(UDF_INIT *initid, UDF_ARGS *args, char *message, bool need_result, const char* function_name) {
-  if (args->arg_count == 0) {
-    sprintf(message,"Wrong arguments to %s();  Must have at least 1 argument", function_name);
-    return 1;
-  }
+my_bool merge_init(UDF_INIT *initid, UDF_ARGS *args, char *message,
+		bool need_result, const char* function_name) {
+	if (args->arg_count == 0) {
+		sprintf(message,
+				"Wrong arguments to %s();  Must have at least 1 argument",
+				function_name);
+		return 1;
+	}
 
-  for(int i = 0; i < args->arg_count; ++i) {
-    if (args->arg_type[i] != STRING_RESULT) {
-      sprintf(message,"Wrong arguments to %s();  All arguments must be of type string", function_name);
-      return 1;    
-    }
-  }
+	for (int i = 0; i < args->arg_count; ++i) {
+		if (args->arg_type[i] != STRING_RESULT) {
+			sprintf(message,
+					"Wrong arguments to %s();  All arguments must be of type string",
+					function_name);
+			return 1;
+		}
+	}
 
-  initid->ptr = (char*)new Data(need_result);
-  return 0;
+	initid->ptr = (char*) new Data(need_result);
+	return 0;
 }
 
 my_bool EXPORT hll_merge_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
-  return merge_init(initid, args, message, true, "HLL_MERGE");
+	return merge_init(initid, args, message, true, "HLL_MERGE");
 }
 
 void EXPORT hll_merge_deinit(UDF_INIT *initid) {
-  delete data(initid);
+	delete data(initid);
 }
 
 char EXPORT *hll_merge(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error) {
-  LOG("hll_merge\n");
-  return hll_create(initid, args, result, length, is_null, error);
+		unsigned long *length, char *is_null, char *error) {
+	LOG("hll_merge\n");
+	return hll_create(initid, args, result, length, is_null, error);
 }
 
 void EXPORT hll_merge_clear(UDF_INIT* initid, char* is_null, char* message) {
-  LOG("hll_merge_clear\n");
-  shll(initid)->clear();
+	LOG("hll_merge_clear\n");
+	shll(initid)->clear();
 }
 
-void EXPORT hll_merge_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
-  for(int i = 0; i < args->arg_count; ++i) {
-    LOG("hll_merge_add %d\n", i);
+void EXPORT hll_merge_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null,
+		char* message) {
+	for (int i = 0; i < args->arg_count; ++i) {
+		LOG("hll_merge_add %d\n", i);
 
-    uint32_t length;
-    const char* arg;
-    get_value_and_length(args, i, &arg, &length);
-    if (length == 0) continue; // NULL handling
-    
-    LOG("hll_add %.*s %d\n", (int)length, arg, (int)length);
-    char* hll_str = (char*)malloc(length + 1);
-    
-    strncpy(hll_str, arg, length);
-    hll_str[length] = '\0';
+		uint32_t length;
+		const char* arg;
+		get_value_and_length(args, i, &arg, &length);
+		if (length == 0)
+			continue; // NULL handling
 
-    SerializedHyperLogLog* current_shll = SerializedHyperLogLog::fromString(hll_str);
-    free(hll_str);
+		LOG("hll_add %.*s %d\n", (int )length, arg, (int )length);
+		char* hll_str = (char*) malloc(length + 1);
 
-    if (current_shll != NULL) {
-      shll(initid)->merge(*current_shll);
-      delete current_shll;
-    }
-  }
+		strncpy(hll_str, arg, length);
+		hll_str[length] = '\0';
+
+		SerializedHyperLogLog* current_shll = SerializedHyperLogLog::fromString(
+				hll_str);
+		free(hll_str);
+
+		if (current_shll != NULL) {
+			shll(initid)->merge(*current_shll);
+			delete current_shll;
+		}
+	}
 }
 
-my_bool EXPORT hll_merge_compute_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
-  return merge_init(initid, args, message, false, "HLL_MERGE_COMPUTE");
+my_bool EXPORT hll_merge_compute_init(UDF_INIT *initid, UDF_ARGS *args,
+		char *message) {
+	return merge_init(initid, args, message, false, "HLL_MERGE_COMPUTE");
 }
 
 void EXPORT hll_merge_compute_deinit(UDF_INIT *initid) {
-  hll_merge_deinit(initid);
+	hll_merge_deinit(initid);
 }
 
-long long EXPORT hll_merge_compute(UDF_INIT *initid, UDF_ARGS *args, char *result,
-          unsigned long *length, char *is_null, char *error) {
-  return shll(initid)->estimate();
+long long EXPORT hll_merge_compute(UDF_INIT *initid, UDF_ARGS *args,
+		char *result, unsigned long *length, char *is_null, char *error) {
+	return shll(initid)->estimate();
 }
 
-void EXPORT hll_merge_compute_clear(UDF_INIT* initid, char* is_null, char* message) {
-  hll_merge_clear(initid, is_null, message);
+void EXPORT hll_merge_compute_clear(UDF_INIT* initid, char* is_null,
+		char* message) {
+	hll_merge_clear(initid, is_null, message);
 }
 
-void EXPORT hll_merge_compute_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* message) {
-  hll_merge_add(initid, args, is_null, message);
+void EXPORT hll_merge_compute_add(UDF_INIT* initid, UDF_ARGS* args,
+		char* is_null, char* message) {
+	hll_merge_add(initid, args, is_null, message);
 }
 
 }
